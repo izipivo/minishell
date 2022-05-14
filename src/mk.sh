ad7bf4c56a1b879ceb4731f884a104a272975fda
ivan3scence <ivan.gorich@gmail.com>
2022-05-15 00:40:33 +0300

    mk

--- src/mk.sh
+++ src/mk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #MacOS
-cc -fsanitize=address -L../libft -lft -L /usr/include -lreadline env_list.c parser.c -o parser&&./parser
-cc -L../libft -lft -L /usr/include -lreadline env_list.c parser.c -o parser&&./parser
+cc -Werror -Wextra -Wall -fsanitize=address -L../libft -lft -L /usr/include -lreadline env_list.c parser.c -o parser&&./parser
+cc -Werror -Wextra -Wall -L../libft -lft -L /usr/include -lreadline env_list.c parser.c -o parser&&./parser
 #linux
-cc -fsanitize=address env_list.c parser.c -o parser -L../libft -lft -L /usr/include -lreadline&&./parser
-cc env_list.c parser.c -o parser -L../libft -lft -L /usr/include -lreadline&&./parser
+cc -Werror -Wextra -Wall -fsanitize=address env_list.c parser.c -o parser -L../libft -lft -L /usr/include -lreadline&&./parser
+cc -Werror -Wextra -Wall env_list.c parser.c -o parser -L../libft -lft -L /usr/include -lreadline&&./parser
