e390282cde2782586ef7db782817c3eeb7ab2a14
Superintendent Donny <sdonny@at-l5.msk.21-school.ru>
2022-06-18 17:34:41 +0300

    Fix: echo fixed

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -18,14 +18,20 @@ char	*ft_strjoin(char const *s1, char const	*s2)
 	int		i;
 
 	if (!s1 || !s2)
+	{
+		// ft_putstr_fd("ft_strjoin: no input strings\n", 2);
 		return (0);
+	}
 	i = -1;
 	if (!s1)
 		joined = (char *) malloc(ft_strlen(s2) + 1);
 	else
 		joined = (char *) malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (!joined)
+	{
+		ft_putstr_fd("ft_strjoin: malloc rip\n", 2);
 		return (0);
+	}
 	while (s1 && *s1)
 		joined[++i] = *s1++;
 	while (s2 && *s2)
